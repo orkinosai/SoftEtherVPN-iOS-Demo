d6c53acb1ff204458839c17b507c7a30c5f24679
orkinosai <ismail@orkinosai.com>
2025-06-25 22:06:02 +0000

    added file
    file added

---
+++ src/Cedar/GlobalConst.h
@@ -0,0 +1,12 @@
+#ifndef GLOBALCONST_H
+#define GLOBALCONST_H
+
+// Define some common global constants used across the Cedar sources.
+// You can expand this as needed if you get missing symbol errors.
+
+#define MAX_PATH 260
+#define INFINITE 0xFFFFFFFF
+#define DEFAULT_PORT 443
+#define DEFAULT_TIMEOUT 30000
+
+#endif // GLOBALCONST_H
